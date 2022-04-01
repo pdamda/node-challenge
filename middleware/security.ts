ee340b9eddd7d93e8fee1ec536bfbc30d4304f0b
Petar Scepanovic <petar.scepanovic@bluegrid.io>
2022-04-01 20:18:23 +0200

    update

--- middleware/security.ts
+++ middleware/security.ts
@@ -1,9 +1,10 @@
 export default function security(req, res, next) {
-  res.removeHeader('X-Powered-By');
+  res.removeHeader("X-Powered-By");
 
-  if (decodeURIComponent(req.url).includes('<script>')) {
-    return res.status(406).end('Illegal component in URI');
+  if (decodeURIComponent(req.url).includes("<script>")) {
+    return res.status(406).end("Illegal component in URI");
   }
 
+  console.log("hello");
   next();
 }
